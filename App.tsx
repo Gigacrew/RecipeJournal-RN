bd343ccf0b6d6006a84b4e8e89195e05890e0f1d
Jaivleenkour <jaivleenkour11@gmail.com>
2023-07-06 21:44:10 -0400

    Added Recipe Detail Port-78

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import PageHeader from './src/components/Header';
+import RecipeDetail from './src/components/RecipeDetail';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <PageHeader></PageHeader>
+      <RecipeDetail></RecipeDetail>
     </View>
   );
 }
@@ -13,8 +15,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#46288b'
   },
 });

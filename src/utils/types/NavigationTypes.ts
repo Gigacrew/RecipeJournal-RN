0d15c90b5115711ddec2a553d8ccc890c2cd2b82
Eric Donnelly <200433532@student.georgianc.on.ca>
2023-07-09 18:51:57 -0400

    Created MealPlansDetails Component, Checkbox Component, Modified NavTypes and Bar to accept new values PORT-76

--- src/utils/types/NavigationTypes.ts
+++ src/utils/types/NavigationTypes.ts
@@ -2,17 +2,25 @@ import { RouteProp } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 export type RootStackParamList = {
-  Home: HomeScreenCustomProps;
-  Details: DetailsScreenCustomProps;
-  Recipe: RecipeDetailCustomProps;
+  Home?: HomeScreenCustomProps;
+  Details?: DetailsScreenCustomProps;
+  Recipe?: RecipeDetailCustomProps;
+  MealPlanDetails?: MealPlanDetailsScreenCustomProps;
 };
 
 type HomeScreenCustomProps = {
   navButtonText: string;
   title?: string;
+  text?: string;
 };
 type DetailsScreenCustomProps = {
   navButtonText: string;
+  text?: string;
+  title?: string;
+};
+type MealPlanDetailsScreenCustomProps = {
+  navButtonText: string;
+  text?: string;
   title?: string;
 };
 type RecipeDetailCustomProps = {
@@ -36,9 +44,22 @@ type RecipeScreenNavigationProp = NativeStackNavigationProp<
   RootStackParamList,
   "Recipe"
 >;
+type MealPlanDetailsScreenRouteProp = RouteProp<
+  RootStackParamList,
+  "MealPlanDetails"
+>;
+type MealPlanDetailsScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  "MealPlanDetails"
+>;
+
 export type DetailsScreenProps = {
   navigation: DetailsScreenNavigationProp;
   route: DetailsScreenRouteProp;
+};
+export type MealPlanDetailsScreenProps = {
+  navigation: MealPlanDetailsScreenNavigationProp;
+  route: MealPlanDetailsScreenRouteProp;
 };
 export type HomeScreenProps = {
   navigation: HomeScreenNavigationProp;
